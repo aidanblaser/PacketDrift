43dd6a7ac52e1039988b80b92ebc4a6d61dd871b
Aidan Blaser <ablaser@ucsd.edu>
2022-08-22 14:32:49 -0700

    New packages

--- scripts/maxVelocity.jl
+++ scripts/maxVelocity.jl
@@ -8,8 +8,27 @@ of the drift occur.
 Written by: Aidan Blaser (ablaser@ucsd.edu)
 Last Edited: 8/22/2022
 =#
+using DrWatson
+@quickactivate "PacketDrift"
+using Plots
+using Distributions
+
 
 # Make sure you run DoldSim.jl first to get simulation
 # want x_o and y_o
 
-u = diff(x_o,di)./(t[2]-t[1]);
+u = copy(x_o);
+
+for i ∈ 1:(length(t)-1)
+    u[i,:] = (x_o[i+1,:]-x_o[i,:])/(t[i+1]-t[i]);
+end
+
+@gif for i ∈ 1:2:length(t)
+    time = round(T.*t[i],digits=3);
+    plot(x_o[i,:].*L, u[i,:].*L./T, xlims=(0,wl),
+    ylims = (-0.15,0.15), xlabel="x (m)",ylabel="u (m/s)",
+    title = "S = $S , Δ = $D, t = $time s")
+end 
+
+
+histogram2d(u[:],y_o[:])
